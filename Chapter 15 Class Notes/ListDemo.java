d4a21af7fdfed773c40ebef91daf9f66ff750c94
Geoff Schmit <gcschmit@gmail.com>
2017-09-08 14:13:20 -0500

    Started ListDemo class notes. Start with iterator on Monday.

--- Chapter 15 Class Notes/ListDemo.java
+++ Chapter 15 Class Notes/ListDemo.java
@@ -8,24 +8,32 @@ public class ListDemo
 {
    public static void main(String[] args)
    {
+       LinkedList<String> staff = new LinkedList<>();
+       staff.addLast( "Diana" );
+       staff.addLast( "Harry" );
+       staff.addLast( "Romeo" );
+       staff.addLast( "Tom" );
+       
+       // the list is currently: DHRT
 
+       // | in the comments indicates the iterator position
+       
+       ListIterator<String> iterator = staff.listIterator();    // |DHRT
 
-      // | in the comments indicates the iterator position
-
-					// |DHRT
-       				// D|HRT
-      				// DH|RT
+                    // |DHRT
+                    // D|HRT
+                    // DH|RT
 
       // Add more elements after second element
 
-      				// DHJ|RT
-      				// DHJN|RT
+                    // DHJ|RT
+                    // DHJN|RT
 
-      				// DHJNR|T
+                    // DHJNR|T
 
       // Remove last traversed element
 
-      				// DHJN|T
+                    // DHJN|T
 
       // Print all elements
 
