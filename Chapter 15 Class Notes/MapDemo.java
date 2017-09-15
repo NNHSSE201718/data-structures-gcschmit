234a0babf86f032d975575dd9c67cad95050b705
Geoff Schmit <gcschmit@gmail.com>
2017-09-15 12:32:07 -0500

    implemented the MapDemo class

--- Chapter 15 Class Notes/MapDemo.java
+++ Chapter 15 Class Notes/MapDemo.java
@@ -4,23 +4,31 @@ import java.util.Map;
 import java.util.Set;
 
 /**
-   This program demonstrates a map that maps names to colors.
-*/
+This program demonstrates a map that maps names to colors.
+ */
 public class MapDemo
 {
-   public static void main(String[] args)
-   {
-	   //create Map: favoriteColors
+    public static void main(String[] args)
+    {
+        //create Map: favoriteColors
+        /*
+         * The Map interface is a generic. The first type is the type of the
+         *      key; the second type, the value.
+         */
+        Map<String, Color> favoriteColors = new HashMap<>();
+        
+        favoriteColors.put( "Nicole", Color.BLUE );
+        favoriteColors.put( "Muhammad", Color.GREEN );
+        favoriteColors.put( "Preston", Color.ORANGE );
+        favoriteColors.put( "Richard", Color.ORANGE );
 
-
-	  //create Set<String> keySet
-
-      // Print all keys and values in the map
-
-
-      for ()
-      {
-
-      }
-   }
+        //create Set<String> keySet
+        Set<String> keySet = favoriteColors.keySet();
+        
+        // Print all keys and values in the map
+        for( String key : keySet )
+        {
+            System.out.println( key + ": " + favoriteColors.get( key ));
+        }
+    }
 }
