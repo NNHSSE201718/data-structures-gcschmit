16ae7cd1150497fa56c3e107dce75056badd139b
Geoff Schmit <gcschmit@gmail.com>
2017-09-28 14:55:29 -0500

    finished LinkedList class

--- Chapter 16 Class Notes/ListDemo.java
+++ Chapter 16 Class Notes/ListDemo.java
@@ -1,32 +1,36 @@
 /**
-   A program that demonstrates the LinkedList class
-*/
+A program that demonstrates the LinkedList class
+ */
 public class ListDemo
 {
-   public static void main(String[] args)
-   {
-	  //create LinkedList and add to it
-      LinkedList staff = new LinkedList();
+    public static void main(String[] args)
+    {
+        //create LinkedList and add to it
+        LinkedList staff = new LinkedList();
+        staff.addFirst( "Tom" );
+        staff.addFirst( "Romeo" );
+        staff.addFirst( "Harry" );
+        staff.addFirst( "Diana" );
 
+        // | in the comments indicates the iterator position
+        ListIterator iterator = staff.listIterator();   // |DHRT
+        iterator.next();                                // D|HRT
+        iterator.next();                                // DH|RT
 
-      // | in the comments indicates the iterator position
+        // Add more elements after second element
+        iterator.add( "Juliet" );                       // DHJ|RT
+        iterator.add( "Nina" );                         // DHJN|RT
 
+        // Remove Romeo
+        iterator.next();                                // DHJNR|T
+        iterator.remove();                              // DHJN|T
 
-      // Add more elements after second element
-
-
-
-      // Remove last traversed element
-
-
-
-      // Print all elements
-
-      iterator = staff.listIterator();
-      while (iterator.hasNext())
-      {
-         System.out.print(iterator.next() + " ");
-      }
-      System.out.println();
-   }
+        // Print all elements
+        iterator = staff.listIterator();
+        while( iterator.hasNext())
+        {
+            System.out.print( iterator.next() + " " );
+        }
+        System.out.println();
+    }
 }
