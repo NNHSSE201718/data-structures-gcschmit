6c1f81933f5c24f41bcefe96d2d1fe81cf6b6857
Geoff Schmit <gcschmit@gmail.com>
2017-10-24 14:12:03 -0500

    implemented start of Tree class

--- Chapter 17 Class Notes/Tree.java
+++ Chapter 17 Class Notes/Tree.java
@@ -2,59 +2,75 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
-   A tree in which each node has an arbitrary number of children.
-*/
+A tree in which each node has an arbitrary number of children.
+ */
 public class Tree
 {
-   
+    private Node root;
 
-   class Node
-   {
-      
+    class Node
+    {
+        public Object data;
+        public List<Node> children;
 
-      /**
-         Computes the size of the subtree whose root is this node.
-         @return the number of nodes in the subtree
-      */
-      public int size()
-      {
-         
-      }
-   }
+        /**
+        Computes the size of the subtree whose root is this node.
+        @return the number of nodes in the subtree
+         */
+        public int size()
+        {
+            int sum = 0;
+            for( Node child : this.children )
+            {
+                sum += child.size();
+            }
+            
+            return sum + 1;
+        }
+    }
 
-   /**
-      Constructs an empty tree.
-   */
-   public Tree()
-   {
-      
-   }
+    /**
+    Constructs an empty tree.
+     */
+    public Tree()
+    {
+        this.root = null;
+    }
 
-   /**
-      Constructs a tree with one node and no children.
-      @param rootData the data for the root
-   */
-   public Tree(Object rootData)
-   {
-      
-   }
+    /**
+    Constructs a tree with one node and no children.
+    @param rootData the data for the root
+     */
+    public Tree(Object rootData)
+    {
+        this.root = new Node();
+        this.root.data = rootData;
+        this.root.children = new ArrayList<>();
+    }
 
-   /**
-      Adds a subtree as the last child of the root.
-   */
-   public void addSubtree(Tree subtree)
-   {
-      
-   }
+    /**
+    Adds a subtree as the last child of the root.
+     */
+    public void addSubtree(Tree subtree)
+    {
+        this.root.children.add( subtree.root );
+    }
 
-   /**
-      Computes the size of this tree.
-      @return the number of nodes in the tree
-   */
-   public int size() 
-   {
-      
-   }
+    /**
+    Computes the size of this tree.
+    @return the number of nodes in the tree
+     */
+    public int size() 
+    {
+        if( this.root == null )
+        {
+            return 0;
+        }
+        else
+        {
+            return this.root.size();
+        }
+    }
 
-   // Additional methods will be added in later sections.
+    // Additional methods will be added in later sections.
 }
