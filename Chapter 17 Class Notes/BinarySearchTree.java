2aa9f45b3d8fc8937cb7852c587dc021cd24050a
Geoff Schmit <gcschmit@gmail.com>
2017-11-02 15:09:11 -0500

    finished BinarySearchTree remove method

--- Chapter 17 Class Notes/BinarySearchTree.java
+++ Chapter 17 Class Notes/BinarySearchTree.java
@@ -106,7 +106,60 @@ public class BinarySearchTree
         // toBeRemoved refers to the element to remove
         
         // if one of the children is empty, use the other child
+        //      Case 1 & 2
+        if( toBeRemoved.left == null || toBeRemoved.right == null )
+        {
+            Node newChild;
+            if( toBeRemoved.left == null )
+            {
+                newChild = toBeRemoved.right;
+            }
+            else
+            {
+                newChild = toBeRemoved.left;
+            }
+            
+            if( parent == null )
+            {
+                root = newChild;
+            }
+            else if( parent.left == toBeRemoved )
+            {
+                parent.left = newChild;
+            }
+            else
+            {
+                parent.right = newChild;
+            }
+            
+            return;
+        }
         
+        // neither subtree of the node to be removed is empty (case 3)
+        
+        // find the smallest element of the right subtree
+        Node smallestParent = toBeRemoved;
+        Node smallest = toBeRemoved.right;
+        while( smallest.left != null )
+        {
+            smallestParent = smallest;
+            smallest = smallest.left;
+        }
+        
+        // smallest contains the smallest child in the right subtree
+        
+        // move data
+        toBeRemoved.data = smallest.data;
+        
+        // unlink child
+        if( smallestParent == toBeRemoved )
+        {
+            smallestParent.right = smallest.right;
+        }
+        else
+        {
+            smallestParent.left = smallest.right;
+        }
     }
 
     /**
